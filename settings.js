a886eee876cb7e5cef0e85f68086e2ba72db69ef
QUEENSIA <queensia254@gmail.com>
2025-08-23 15:51:07 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐒𝐩𝐚𝐫𝐭𝐚-𝐁𝐨𝐭',
+  author: '𝐒𝐩𝐚𝐫𝐭𝐚-𝟐𝟖',
+  botName: "𝐒𝐏𝐀𝐑𝐓𝐀-𝐗𝐌𝐃",
+  botOwner: '𝐒𝐩𝐚𝐫𝐭𝐚-𝟐𝟖', // Your name
+  ownerNumber: '254795452444', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
